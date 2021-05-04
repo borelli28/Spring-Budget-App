cf6c041885d416b4bdf1f266663c21bba97830a0
Armando <borelliarmando12@gmail.com>
2021-05-04 04:53:50 -0500

    Added first test case for User model

--- myBudget/src/main/java/com/armando/myBudget/services/UserService.java
+++ myBudget/src/main/java/com/armando/myBudget/services/UserService.java
@@ -1,31 +1,33 @@
 package com.armando.myBudget.services;
 
+import org.springframework.stereotype.Service;
+
 @Service
 public class UserService {
 	
-    private UserRepo userRepo;
-    private RoleRepo roleRepo;
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-    
-    public UserService(UserRepo userRepo, RoleRepo roleRepo, BCryptPasswordEncoder bCryptPasswordEncoder)     {
-    	this.userRepo = userRepo;
-    	this.roleRepo = roleRepo;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-    }
-    // Saves a client with only the user role.
-    public void saveWithUserRole(User user) {
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        user.setRoles(roleRepo.findByName("ROLE_USER"));
-        userRepo.save(user);
-    }
-    // Saves a client with only the admin role.
-    public void saveUserWithAdminRole(User user) {
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        user.setRoles(roleRepo.findByName("ROLE_ADMIN"));
-        userRepo.save(user);
-    }    
-    // Finds a user by their username.
-    public User findByUsername(String username) {
-        return userRepo.findByUsername(username);
-    }
+//    private UserRepo userRepo;
+//    private RoleRepo roleRepo;
+//    private BCryptPasswordEncoder bCryptPasswordEncoder;
+//    
+//    public UserService(UserRepo userRepo, RoleRepo roleRepo, BCryptPasswordEncoder bCryptPasswordEncoder)     {
+//    	this.userRepo = userRepo;
+//    	this.roleRepo = roleRepo;
+//        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+//    }
+//    // Saves a client with only the user role.
+//    public void saveWithUserRole(User user) {
+//        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+//        user.setRoles(roleRepo.findByName("ROLE_USER"));
+//        userRepo.save(user);
+//    }
+//    // Saves a client with only the admin role.
+//    public void saveUserWithAdminRole(User user) {
+//        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+//        user.setRoles(roleRepo.findByName("ROLE_ADMIN"));
+//        userRepo.save(user);
+//    }    
+//    // Finds a user by their username.
+//    public User findByUsername(String username) {
+//        return userRepo.findByUsername(username);
+//    }
 }
--- myBudget/src/test/java/com/armando/myBudget/MyBudgetApplicationTests.java
+++ myBudget/src/test/java/com/armando/myBudget/MyBudgetApplicationTests.java
@@ -1,13 +1,66 @@
 package com.armando.myBudget;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.armando.myBudget.controllers.UserController;
+import com.armando.myBudget.models.User;
+
+@SpringBootTest // test class
 class MyBudgetApplicationTests {
 
-	@Test
+	@Autowired
+	private UserController userController;
+	
+	private static Validator validator;
+	
+	@BeforeAll
+	static void setUp() {
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		validator = factory.getValidator();
+	}
+	
+	
+	@Test // test case
 	void contextLoads() {
+	}
+	
+	@Test
+	void testUserController() {
+		assertThat(userController).isNotNull();
+	}
+	
+	// Creates an empty instance of the User model and pass it to the validate method of the validator. 
+	// Any violations of the validations we have set on the models,
+	// will be returned as a Set.
+	// If there is any violations, we can iterate through them and print them to the console.
+	@Test
+	void testUserModel() {
+		User user = new User();
+		
+		user.setFirstName("Joe");
+		user.setLastName("Mama");
+		user.setEmail("some@email.com");
+		user.setPassword("somepassword");
+		user.setPasswordConfirmation("somepassword");
+		
+		Set<ConstraintViolation<User>> violations = validator.validate(user);
+		for (ConstraintViolation<User> violation : violations) {
+			System.out.println(violation.getMessage());
+		}
+		assertTrue(violations.isEmpty());
 	}
 
 }
